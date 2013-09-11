0fcfa39f01baf8cd3253f10f3e3842f1a64e327b
Philippe Wang <philippe.wang@gmail.com>
2013-09-11 16:46:31 +0100

    fix

--- ocamlapplet.bash
+++ ocamlapplet.bash
@@ -17,14 +17,13 @@ function hash() {
     cat | (md5 || md5sum) | sed -e 's| ./*||g'
 }
 
-echo -n "<script>ml$(id "$1") = '$(./htmlescape < $tmpfile.ml)';</script><span style='position:relative;bottom:-1em;font-size:80%;'><span onclick='alert(\"not yet implemented\")'>[try]</span><span onclick='alert(\"not yet implemented\")'>[copy]</span><span onclick='alert(\"not yet implemented\")'>[github]</span></span>" > $tmpfile.html
-
 if which -s ocamltohtml
 then
-    ocamltohtml < $tmpfile.ml >> $tmpfile.html
+    ocamltohtml < $tmpfile.ml > $tmpfile.html
 else
-    ./ocamltohtml < $tmpfile.ml >> $tmpfile.html
+    ./ocamltohtml < $tmpfile.ml > $tmpfile.html
 fi
+echo -n "<script>ml$(id "$1") = '$(./htmlescape < $tmpfile.ml)';</script><span style='position:relative;bottom:-1em;font-size:80%;'><span onclick='alert(\"not yet implemented\")'>[try]</span><span onclick='alert(\"not yet implemented\")'>[copy]</span><span onclick='alert(\"not yet implemented\")'>[github]</span></span>" >> $tmpfile.html
 
 cat $tmpfile.html
 
