76797a402d5619e53c81efa201321cf5624f4e96
Philippe Wang <philippe.wang@gmail.com>
2013-09-11 15:54:03 +0100

    genocamlapplet.bash (draft)

---
+++ genocamlapplet.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+tmpfile=$(date +tmp%Y%m%d%H%M%S%N)_$RANDOM$RANDOM$RANDOM
+
+cat > $tmpfile.ml
+
+function hash() {
+    cat | (md5 || md5sum) | sed -e 's| ./*||g'
+}
+
+echo -n "<script>val ml$(hash < $tmpfile.ml) = '$(./htmlescape < $tmpfile.ml)';</script>" > $tmpfile.html
+
+if which -s ocamltohtml
+then
+    ocamltohtml < $tmpfile.ml >> $tmpfile.html
+else
+    ./ocamltohtml < $tmpfile.ml >> $tmpfile.html
+fi
+
+cat $tmpfile.html
+
+rm -f $tmpfile*
+
